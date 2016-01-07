ed7a27f1d0401c8f82725328fbd4a844ab324c55
Kyle Whittington <kthomaswhittington@gmail.com>
2016-01-07 08:16:53 -0600

    Add template registration to lib/yard/spec.rb

--- lib/yard/spec.rb
+++ lib/yard/spec.rb
@@ -1,5 +1,8 @@
 require 'yard/spec/version'
 
+YARD::Templates::Engine.register_template_path(
+  Pathname.new(YARD::ROOT).join('..', 'templates_custom'))
+
 module YARD
   # Embeds unit specifications in YARD output.
   module Spec
